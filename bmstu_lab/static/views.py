57befc614549bb3f23b5952228ebd5c39b5d1c3e
stella <stelluchchka@gmail.com>
2023-09-13 00:17:16 +0000

    edit style

--- bmstu_lab/static/views.py
+++ bmstu_lab/static/views.py
@@ -1,23 +1,21 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import date
-
+orders = [
+            {'title': 'бургер', 'id': 0, 'chef': 'Mauro Colagreco', 'price': '9', 'tegs': 'VEG', 'url': 'https://cojo.ru/wp-content/uploads/2022/12/risunki-edy-1.webp'},
+            {'title': 'не бургер', 'id': 1, 'chef': 'Christophe Bacquie', 'price': '15', 'tegs': 'VEG', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
+            {'title': 'неxnj', 'id': 2, 'chef': 'Mauro Colagreco', 'price': '4', 'tegs': 'SPICY', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
+            {'title': 'неxnj', 'id': 3, 'chef': 'Laurent Petit', 'price': '9', 'tegs': 'NEW', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
+            {'title': 'неxnj', 'id': 4, 'chef': 'Mauro Colagreco', 'price': '4', 'tegs': 'SWEET, SPICY', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
+            {'title': 'Коврик для мышки',  'id': 5, 'chef': 'Dan Barber', 'price': '19', 'tegs': 'VEG', 'url': 'https://media.istockphoto.com/id/1136168094/ru/фото/куриные-терияки-еды-подготовительный-обед-коробки-контейнеры-с-брокколи-рисом-и-морковью.jpg?s=612x612&w=0&k=20&c=7oRrk2U0oKLKuviPV2LBYpNWbS0_n0bOwQbMfALgz_I='},
+        ]
 def GetOrders(request):
     return render(request, 'orders.html', {'data' : {
-        'current_date': date.today(),
-        'orders': [
-            {'title': 'бургер', 'id': 1, 'chef': 'Mauro Colagreco', 'tegs': 'VEG', 'url': 'https://cojo.ru/wp-content/uploads/2022/12/risunki-edy-1.webp'},
-            {'title': 'не бургер', 'id': 2, 'chef': 'Christophe Bacquie', 'tegs': 'VEG', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
-            {'title': 'неxnj', 'id': 3, 'chef': 'Mauro Colagreco', 'tegs': 'SPICY', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
-            {'title': 'неxnj', 'id': 4, 'chef': 'Laurent Petit', 'tegs': 'NEW', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
-            {'title': 'неxnj', 'id': 5, 'chef': 'Mauro Colagreco', 'tegs': 'SWEET, SPICY', 'url': 'https://img.freepik.com/premium-vector/food-and-dessert-seamless-pattern_1639-40641.jpg'},
-            {'title': 'Коврик для мышки',  'id': 6, 'chef': 'Dan Barber', 'tegs': 'VEG', 'url': 'https://media.istockphoto.com/id/1136168094/ru/фото/куриные-терияки-еды-подготовительный-обед-коробки-контейнеры-с-брокколи-рисом-и-морковью.jpg?s=612x612&w=0&k=20&c=7oRrk2U0oKLKuviPV2LBYpNWbS0_n0bOwQbMfALgz_I='},
-        ]
+        'orders': orders
     }})
 
 def GetOrder(request, id):
     return render(request, 'order.html', {'data' : {
-        'current_date': date.today(),
         'id': id
     }})
 
