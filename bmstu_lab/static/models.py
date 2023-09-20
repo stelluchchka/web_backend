d1c735f27e05e2679e50b103f40e3ac449a22b91
stelluchchka <stelluchchka@gmail.com>
2023-09-20 08:41:27 +0300

    started

--- bmstu_lab/static/models.py
+++ bmstu_lab/static/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+class Dishes(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    tags = models.CharField(max_length=255, null=True, blank=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    energy_value = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    expiry_date = models.DateField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    chef_name = models.CharField(max_length=255, null=False)
+    chef_post = models.CharField(max_length=255, null=True, blank=True)
+    chef_url = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class Orders(models.Model):
+    STATUS_CHOICES = (
+        ('registered', 'введен'),
+        ('moderating', 'готовиться'),
+        ('approved', 'приготовлен'),
+        ('denied', 'отменен'),
+        ('deleted', 'eдален')
+    )
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='CREATED')
+    created_at = models.DateTimeField(auto_now_add=True)
+    processed_at = models.DateTimeField(null=True, blank=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    moderator = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.status}"
+
+class DishesOrders(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dishes, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.dish.title}"
+
+class Users(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
--- lab/settings.py
+++ lab/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'lab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
